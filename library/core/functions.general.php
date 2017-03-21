e0448130d4da04232225f12ae7b1d330079c115b
beckyvb <beckyvanbussel@gmail.com>
2017-03-21 17:28:48 -0400

    Add an optional site setting to send users to /leaving when clicking on a ugc link
    * Breaks the embed code replacement into its own function `embedReplacement($url)`, and optimizes and refactors the embed code replacement.
    * Adds a `matchesTrustedDomain($url)` function to test whether a given url matches a trusted domain in the `linksCallback` function.
    * Adds a redirect to `/leaving` to any non-trusted link if the `Garden.Format.WarnLeaving` config is set.
    * Adds a `reverse` param to the toggle to handle "disable"-type config settings.
    * Adds controls for the `Garden.Format.WarnLeaving` and `Garden.Format.DisableUrlEmbeds` config settings to the Advanced settings page in the dashboard.
    * Adds a title to the `/leaving` page.
    
    Bonus:
    * Fixes double-urling in the SafeRedirect function.
    * Fixes Wistia and Twitch embeds
    
    Closes https://github.com/vanilla/vanilla/issues/5334

--- library/core/functions.general.php
+++ library/core/functions.general.php
@@ -3079,6 +3079,27 @@ if (!function_exists('safeImage')) {
     }
 }
 
+if (!function_exists('matchesTrustedDomain')) {
+    /**
+     * Tests whether a given url matches one of our trusted domains.
+     *
+     * @param string $url The url to test.
+     * @return bool Whether the url matches a trusted domain.
+     */
+    function matchesTrustedDomain($url = '') {
+        $url = url($url, true);
+        $trustedDomains = trustedDomains();
+
+        foreach ($trustedDomains as $trustedDomain) {
+            if (urlMatch($trustedDomain, $url)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
 if (!function_exists('safeRedirect')) {
     /**
      * Redirect, but only to a safe domain.
@@ -3087,20 +3108,12 @@ if (!function_exists('safeRedirect')) {
      * @param int $StatusCode The status of the redirect. Defaults to 302.
      */
     function safeRedirect($Destination = false, $StatusCode = null) {
+        $isTrustedDomain = matchesTrustedDomain($Destination);
+
         if (!$Destination) {
             $Destination = Url('', true);
         } else {
             $Destination = Url($Destination, true);
-        }
-
-        $trustedDomains = TrustedDomains();
-        $isTrustedDomain = false;
-
-        foreach ($trustedDomains as $trustedDomain) {
-            if (urlMatch($trustedDomain, $Destination)) {
-                $isTrustedDomain = true;
-                break;
-            }
         }
 
         if ($isTrustedDomain) {
@@ -3110,7 +3123,7 @@ if (!function_exists('safeRedirect')) {
                 'url' => $Destination
             ]);
 
-            redirect(url("/home/leaving?Target=".urlencode($Destination)));
+            redirect("/home/leaving?Target=".urlencode($Destination));
         }
     }
 }
